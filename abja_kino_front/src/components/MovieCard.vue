1ee9b18b5cb7b747cbf12e44301686b465f9b3c7
Taaniel Jakobson <taaniel.jakobson@gmail.com>
2023-05-16 12:00:18 +0300

    AK-4 Alustatud, MovieCard, PosterImage, ScheduleView lisatud

---
+++ abja_kino_front/src/components/MovieCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container border">
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    {{movieInfo.title}}
+                </div>
+                <div class="row">
+                    <PosterImage :image-data-base64="movieInfo.poster"/>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="row">
+                    {{movieInfo.timeStamp}}
+                </div>
+                <div class="row">
+                    Nupp, ValiPilet
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+import PosterImage from "@/components/PosterImage.vue";
+
+export default defineComponent({
+    name: "MovieCard",
+    components: {PosterImage},
+    data() {
+        return {
+            movieInfo: {
+                title: "Pealkiri",
+                poster: "",
+                timeStamp: "Kellaaeg"
+            }
+        }
+    },
+})
+</script>
+
+
