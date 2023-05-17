71c48051cf42bb862e8429ad8c09f2dba902af20
TuuliRei <reiman.tuuli@gmail.com>
2023-05-17 15:05:45 +0300

    Tuuli LoginView

--- .idea/sonarlint/securityhotspotstore/index.pb
+++
@@ -1,3 +0,0 @@
-
-K
-abja_kino_front/src/main.js,3\3\33d11d6719c122ad17fe14167f6adbf383080bba
--- abja_kino_front/src/views/RegisterView.vue
+++ abja_kino_front/src/views/RegisterView.vue
@@ -1,36 +1,47 @@
 <template>
+    <h1>Loo konto</h1>
     <div class="container text-center">
-        <AlertDanger :message="message"/>
+        <AlertSuccess :message="successMessage"/>
+        <AlertDanger :message="errorMessage"/>
         <div @keydown.enter="login" class="row justify-content-center">
             <div class="col col-3 mt-5">
                 <div class="mb-3">
                     <label for="username" class="form-label">Kasutajanimi</label>
-                    <input v-model="username" type="text" class="form-control" id="username">
+                    <input v-model="registrationRequest.username" type="text" class="form-control" id="username">
                 </div>
                 <div class="mb-3">
                     <label for="password" class="form-label">Parool</label>
-                    <input v-model="password" type="password" class="form-control" id="password">
+                    <input v-model="registrationRequest.password" type="password" class="form-control" id="password">
                 </div >
                 <div class="mb-3">
                     <label for="password" class="form-label">Parool uuesti</label>
-                    <input v-model="password" type="password" class="form-control" id="password">
+                    <input v-model="matchingPassword" type="password" class="form-control" id="password">
                 </div >
                 <div class="mb-3">
                     <label for="email" class="form-label">E-posti aadress</label>
-                    <input v-model="email" type="email" class="form-control" id="password">
+                    <input v-model="registrationRequest.email" type="email" class="form-control" id="password">
                 </div >
-
-                <button @click="login" type="submit" class="btn btn-primary">Registreeri</button>
+                <div class="row justify-content-center">
+                    <div class="">
+                         <button @click="navigateBack" type="submit" class="btn btn-outline-success">Tagasi</button>
+                         <button @click="registerNewClient" type="submit" class="btn btn-outline-success">Registreeri</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
     name: "RegisterView",
     data() {
         return {
+            matchingPassword: '',
+            successMessage: "",
+            errorMessage: "",
             registrationRequest: {
                 username: "",
                 password: "",
@@ -38,10 +49,57 @@ export default {
             },
         };
     },
+    methods: {
+        passwordMatch() {
+            return this.registrationRequest.password === this.matchingPassword
+        },
+        navigateBack() {
+            router.push({path: '/'})
+        },
+
+        registerNewClient() {
+            this.errorMessage = ""
+
+
+            if (!this.passwordMatch()) {
+                this.errorMessage = "Paroolid ei kattu"
+                return
+            }
+
+            if(!this.allRequiredFieldsAreFilled()){
+                this.errorMessage = "Täida kõik väljad"
+                return
+            }
+
+            this.postNewClient();
+
+        },
+        allRequiredFieldsAreFilled() {
+            return this.registrationRequest.username !== ''
+                && this.registrationRequest.password !== ''
+                && this.registrationRequest.email !== ''
+
+        },
+        postNewClient() {
+            this.$http.post('/user/register', this.registrationRequest
+                ).then(response => {
+                    this.successMessage = 'Kasutaja loodud' + response.data
+                    this.router.push('/')
+                })
+                .catch(error => {
+                    this.errorResponse = error.response.data
+                    this.handleRegistrationError();
+                })
+
+        },
+        handleRegistrationError() {
+            if (this.errorResponse.code === 400) {
+                this.message = "Kasutajanimi on juba kasutusel"
+            } else {
+                this.message = "Serveri viga"
+            }
+        }
+    }
 
 }
 </script>
-
-<style scoped>
-
-</style>
